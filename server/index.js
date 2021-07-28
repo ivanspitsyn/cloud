53b300f9fb09ccaae0cfc797907f945e3afc4647
Spitsyn Ivan <web@okinavi.ru>
2021-07-29 02:46:38 +0300

    f3

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const authRouter = require('./routes/auth.routes');
+const app = express();
+const PORT = config.get('serverPort');
+
+app.use(express.json());
+app.use('/api/auth', authRouter);
+
+const start = async () => {
+  try {
+    const uri = config.get('dbUrl');
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    app.listen(PORT, () => {
+      console.log('Server is running on port', PORT);
+    });
+  } catch (error) {}
+};
+
+start();
